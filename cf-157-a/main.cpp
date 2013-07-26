5177ce380214a2e830d4a6ba22e35d05b706ba91
Egor Lifar <egor.lifar@gmail.com>
2013-07-26 17:17:50 +0300

    solved problem

--- cf-157-a/main.cpp
+++ cf-157-a/main.cpp
@@ -10,31 +10,30 @@
 using namespace std;
 
 
- 
 int main() {                          
   int n, a[100][100];
   scanf("%d", &n);
-  for(int i =0; i<n; i++){
-    for(int j =0; j<n; j++){
+  for(int i = 0; i < n; i++){
+    for(int j = 0; j < n; j++){
       scanf("%d", &a[i][j]);
     }
   }
-  int k =0;
-   for(int i =0; i<n; i++){
-    for(int j =0; j<n; j++){
-      int s =0;
-      int s2 =0;
-      for(int g =0; g<n; g++){
-         s+=a[g][j];
+  int k = 0;
+   for(int i = 0; i < n; i++){
+    for(int j = 0; j < n; j++){
+      int s = 0;
+      int s2 = 0;
+      for(int g = 0; g < n; g++){
+         s += a[g][j];
       }
-      for(int g =0; g<n; g++){
-         s2+=a[i][g];
+      for(int g = 0; g < n; g++){
+         s2 += a[i][g];
       }
       if(s > s2){
         k++;
       }
     }
   }
-  printf("%d", k);
+  printf("%d\n", k);
   return 0;
 } 
